44debfe9556e473d859440a95c5b8c57b748ded7
yujian <yujian@codvision.com>
2022-11-17 14:52:03 +0800

    2

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ if __name__ == '__main__':
         '7828414': ['煊宝'],
         '7930707': ['朴雨彬']
     }
-
+    room_process_list = []
     for room_id, room_tags in room_map.items():
         process = Process(target=run, args=(room_id, room_tags,))
         process.daemon = True
         process.start()
+        room_process_list.append(process)
+
+    for p in room_process_list:
+        p.join()
